040cc6b4d9f503c06d6c93db23a3601df10168fd
aamaiyaa <74420296+aamaiyaa@users.noreply.github.com>
2023-11-10 17:37:56 -0500

    a5 also

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -17,7 +17,8 @@ function Kanbas() {
   startDate: "2023-09-10",
   endDate: "2023-12-15",
  });
- const URL = "http://localhost:4008/api/courses";
+ const API_BASE = process.env.REACT_APP_API_BASE;
+ const URL = `${API_BASE}/courses`;
 
  const findAllCourses = async () => {
   const response = await axios.get(URL);
